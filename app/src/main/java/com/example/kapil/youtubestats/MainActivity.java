6c0f935926efaaa5237ba147efb272f55dcc8eda
Kapil Kaushik <Kapilkaushik9143@gmail.com>
2021-08-19 18:37:20 +0530

    add internet permission and change theme to day

--- app/src/main/java/com/example/kapil/youtubestats/MainActivity.java
+++ app/src/main/java/com/example/kapil/youtubestats/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCQHLxxBFrbfdrk1jF0moTpw&key=AIzaSyDM9pabe09smtH5laOLBXx4iVL_B-XnrKw
-
+    //https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=love%20babber&type=channel&key=AIzaSyDM9pabe09smtH5laOLBXx4iVL_B-XnrKw
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
